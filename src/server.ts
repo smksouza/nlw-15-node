4004ec9395d2faf87840305ba430c67f99b60844
Samuel Souza <smksouza.dev@gmail.com>
2024-05-28 15:01:17 -0300

    Created base setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,32 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client"
+import { randomUUID } from "crypto";
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 
-app.get('/', () => {
-    return "Hello World"
+
+app.post('/events', async (request: FastifyRequest, reply: FastifyReply) => {
+   const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+   })
+
+    const data = createEventSchema.parse(request.body)
+
+   const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: `fake slug ${randomUUID()}`,
+        }
+    })
+
+    return reply.status(201).send()
 })
 
 app.listen({
