81ec0c1b9506eaa33d69637ff2c9b9d717791b3a
Samuel Souza <smksouza.dev@gmail.com>
2024-06-04 17:29:48 -0300

    Created checkIn route

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -9,10 +9,18 @@ export async function getAttendeeBadge(app: FastifyInstance){
     .get('/attendees/:attendeeId/badge', {
         schema: {
             params: z.object({
-                // attendeeId: z.string().transform(Number)
                 attendeeId: z.coerce.number().int(),
             }),
-            response: {},
+            response: {
+                200: z.object({
+                    badge: z.object({
+                        name: z.string(),
+                        email: z.string().email(),
+                        eventTitle: z.string(),
+                        checkInURL: z.string().url(),
+                    })
+                })
+            },
         }
     }, async (request, reply) => {
         const { attendeeId } = request.params
@@ -36,6 +44,19 @@ export async function getAttendeeBadge(app: FastifyInstance){
             throw new Error('Attende not found.')
         }
 
-        return reply.send({ attendee })
+        const baseURL = `${request.protocol}://${request.hostname}`
+
+        
+
+       const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+        return reply.send({ 
+            badge: {
+                name: attendee.name,
+                email: attendee.email,
+                eventTitle: attendee.event.title,
+                checkInURL: checkInURL.toString(),
+            }
+         })
     })
 }
